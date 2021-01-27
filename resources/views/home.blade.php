f3b62b2290e0f016a7a77d25fbc353a58cb30d96
BuildTools <sarquentin@gmail.com>
2021-01-27 14:28:02 +0100

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>VOICI MON ACCUEIL</h1>
-    <h2>{{ $name }}</h2>
-    <h3> {{ $name == "Quentin Sar" ? "Bonjour Quentin" : "Inconnu" }}</h3>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<h1>VOICI MON ACCUEIL</h1>
+<h2>{{ $name }}</h2>
+<h3> {{ $name == "Quentin Sar" ? "Bonjour Quentin" : "Inconnu" }}</h3>
+@endsection
