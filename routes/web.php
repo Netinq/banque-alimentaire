18a14e840037d5c643fee9f26da1da5e888d7506
BuildTools <sarquentin@gmail.com>
2021-01-27 15:05:36 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'home']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
