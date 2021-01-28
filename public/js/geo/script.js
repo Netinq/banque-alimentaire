04ade2a729c3e1c1494c265b051997407d9aab77
BuildTools <sarquentin@gmail.com>
2021-01-28 01:24:52 +0100

    Update

--- public/js/geo/script.js
+++ public/js/geo/script.js
@@ -1,9 +1,8 @@
 import { geojson } from './locations.js'
 
-mapboxgl.accessToken =
-    "pk.eyJ1IjoibmV0aW5xIiwiYSI6ImNra2N0azJvajBpOWoycHFydWRkcWo5MXMifQ.iCXY_CWx5r8KEOHHCGu3iw";
+mapboxgl.accessToken = "pk.eyJ1IjoibmV0aW5xIiwiYSI6ImNra2N0azJvajBpOWoycHFydWRkcWo5MXMifQ.iCXY_CWx5r8KEOHHCGu3iw";
 
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: "map",
     style: "mapbox://styles/netinq/ckkfj803901yr17p70dmlf1h4",
     center: [-0.57918, 44.837789],
@@ -11,27 +10,29 @@ var map = new mapboxgl.Map({
 });
 
 geojson.features.forEach(function (marker) {
-
     let el = document.createElement("div");
-
     el.className = "marker";
     el.style.width = marker.properties.iconSize[0] + "px";
     el.style.height = marker.properties.iconSize[1] + "px";
-
-    el.addEventListener("click", function () {
-        Array.from(document.getElementsByClassName("marker")).forEach((elem) => {
-            elem.classList.remove("selected");
-        });
+    el.addEventListener("click", () => {
+        Array.from(document.getElementsByClassName("marker")).forEach((elem) => elem.classList.remove('selected'));
         el.classList.add('selected');
         document.getElementById("part_title").innerText = marker.properties.name;
         document.getElementById("part_place").innerText = marker.properties.size;
         document.getElementById("container").classList.remove("hide");
         document.getElementById("container").animate([
             { transform: "translateY(50px)", opacity: 0 },
-            { transform: "translateY(0)", opacity: 1 },], { duration: 300, }
+            { transform: "translateY(0)", opacity: 1 },],
+            { duration: 300, }
         );
-
     });
-
     new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates).addTo(map);
 });
+
+document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("mapboxgl-canvas"))
+    {
+        Array.from(document.getElementsByClassName("marker")).forEach((elem) => elem.classList.remove("selected"));
+        document.getElementById("container").classList.add("hide");
+    }
+})
