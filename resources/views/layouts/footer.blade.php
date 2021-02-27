d862214f8f9fd235e4786f5b1ff8e33a7b1f09d4
Germain-L <germain.leignel@gmail.com>
2021-02-27 14:39:51 +0100

    modified footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,38 @@
-<footer class="col-10 offset-1 row">
-
+<footer class="col-10 offset-1 row py-4 text-center">
+    <div class="col-md-2 py-2">
+        <a href="">
+            <div class="btn">
+                Mentions légales
+            </div>
+        </a>
+    </div>
+    <div class="col-md-2 py-2">
+        <a href="">
+            <div class="btn">
+                RGPD
+            </div>
+        </a>
+    </div>
+    <div class="col-md-2 py-2">
+        <a href="https://www.google.com/maps/search/?api=1&query=25+rue+Bougainville+33300" target="_blank">
+            <div class="btn">
+                <div>25 rue Bougainville</div>
+                <div>33300 Bordeaux Nord</div>
+            </div>
+        </a>
+    </div>
+    <div class="col-md-2 py-2">
+        <a href="">
+            <div class="btn">
+                CGU
+            </div>
+        </a>
+    </div>
+    <div class="col-md-2 py-2">
+        <a href="https://www.banquealimentaire.org/sites/default/files/2020-06/RapportAnnuel_BA2019.pdf" target="_blank">
+            <div class="btn btn-primary">
+                Rapport d'activité
+            </div>
+        </a>
+    </div>
 </footer>
