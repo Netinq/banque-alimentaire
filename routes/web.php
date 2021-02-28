d8c7de56531041b76d5566050e0a8e3ee6ea8318
Germain-L <germain.leignel@gmail.com>
2021-02-28 22:14:27 +0100

    Added legal routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Auth::routes();
 
 Route::get('/panel', [App\Http\Controllers\HomeController::class, 'panel'])->name('panel');
 
+Route::get('/rgpd', [App\Http\Controllers\LegalController::class, 'rgpd'])->name('rgpd');
+Route::get('/cgu', [App\Http\Controllers\LegalController::class, 'cgu'])->name('cgu');
+Route::get('/mention-legales', [App\Http\Controllers\LegalController::class, 'mentions'])->name('mention-legales');
+
 Route::get('/account/create/step1', [App\Http\Controllers\AccountController::class, 'step1'])->name('step1');
 
 Route::resource('volunteer', VolunteerController::class);
